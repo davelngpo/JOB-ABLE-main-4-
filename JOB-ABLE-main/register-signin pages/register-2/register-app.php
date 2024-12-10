fa8cb09212724646f859c6652b84f331a8f03c78
noex08 <mikylam.escudero@gmail.com>
2024-12-10 23:20:57 +0800

    register pages may register na
    incomplete applicant register

---
+++ JOB-ABLE-main/register-signin pages/register-2/register-app.php
@@ -0,0 +1,57 @@
+<?php
+include '../../dbconnect.php';
+
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+    $firstname = $_POST['firstname'];
+    $middlename = $_POST['middlename'];
+    $lastname = $_POST['lastname'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $gender = $_POST['gender'];
+    $age = $_POST['age'];
+    $birth = $_POST['birth'];
+    $contactnumber = $_POST['contactnumber'];
+    $address = $_POST['address'];
+    $edu = $_POST['edu'];
+    $highschool = $_POST['highschool'];
+    $college = $_POST['college'];
+    $tcpp = $_POST['tcpp'];
+
+    $confirm_password = $_POST['confirm_password'];
+
+    if($password !== $confirm_password){
+        header("location: register-page-com.php?error=Passwords do not match!");
+        exit();
+    }
+
+    $stmt = $connect->prepare("SELECT applicant_id FROM applicants WHERE email = ?");
+    $stmt->bind_param("s", $email);
+    $stmt->execute();
+    $stmt->store_result();
+
+    if ($stmt->num_rows > 0) {
+        header("Location: register-page-com.php?error=Email is taken");
+        $stmt->close();
+        $conn->close();
+        exit;
+    }
+    $stmt->close();
+
+    $hash_password = password_hash($password, PASSWORD_BCRYPT);
+
+    $stmt = mysqli_prepare($connect, "INSERT INTO applicants(first_name, middle_name, last_name, gender, age, birth_date, contact_number, email, postal_address, highest_educational_attainment, high_school_name, college_name, password_hash, tcpp) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+
+    mysqli_stmt_bind_param( $stmt, 'ssssissssssssi', $firstname, $middlename, $lastname, $gender, $age, $birth, $contactnumber, $email, $address, $edu, $highschool, $college, $hash_password, $tcpp);
+
+
+    if (mysqli_stmt_execute($stmt)) {
+        header("Location: ../Sign-in.html");
+    } else {
+        echo "<h3>Error: " . $stmt->error . "</h3>";
+    }
+
+    $stmt->close();
+}
+
+$connect->close();
+?>
--- JOB-ABLE-main/register-signin pages/register-2/register-com.php
+++ JOB-ABLE-main/register-signin pages/register-2/register-com.php
@@ -2,55 +2,59 @@
 include '../../dbconnect.php';
 
 if($_SERVER["REQUEST_METHOD"]=="POST"){
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-    $r_name = $_POST['r_name'];
-    $r_email = $_POST['r_email'];
-    $r_number = $_POST['r_number'];
-    $contactnumber = $_POST['contactnumber'];
-    $address = $_POST['add'];
-    $industry = $_POST['industry'];
-    $branch = $_POST['branch'];
-    $description = $_POST['description'];
-    $website = $_POST['website'];
-    $tcpp = $_POST['tcpp'];
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        $r_name = $_POST['r_name'];
+        $r_email = $_POST['r_email'];
+        $r_number = $_POST['r_number'];
+        $contactnumber = $_POST['contactnumber'];
+        $address = $_POST['add'];
+        $industry = $_POST['industry'];
+        $branch = $_POST['branch'];
+        $description = $_POST['description'];
+        $website = $_POST['website'];
+        $tcpp = $_POST['tcpp'];
 
-    $confirm_password = $_POST['confirm_password'];
+        $confirm_password = $_POST['confirm_password'];
 
-    if($password !== $confirm_password){
-        header("location: register-page-com.php?error=Passwords do not match!");
-        exit();
-    }
+        if($password !== $confirm_password){
+            header("location: register-page-com.php?error=Passwords do not match!");
+            exit();
+        }
 
-    $stmt = $connect->prepare("SELECT company_id FROM companies WHERE company_email = ?");
-    $stmt->bind_param("s", $email);
-    $stmt->execute();
-    $stmt->store_result();
+        $stmt = $connect->prepare("SELECT company_id FROM companies WHERE company_email = ?");
+        $stmt->bind_param("s", $email);
+        $stmt->execute();
+        $stmt->store_result();
 
-    if ($stmt->num_rows > 0) {
-        header("Location: register-page-com.php?error=Email is taken");
+        if ($stmt->num_rows > 0) {
+            header("Location: register-page-com.php?error=Email is taken");
+            $stmt->close();
+            $conn->close();
+            exit;
+        }
         $stmt->close();
-        $conn->close();
-        exit;
+
+        $hash_password = password_hash($password, PASSWORD_BCRYPT);
+
+        $stmt = mysqli_prepare($connect, "INSERT INTO companies(company_name, company_email, password, representative_name, representative_email, representative_contact_number, company_contact_number, postal_address, industry_type, branch, company_description, company_website, agreed_terms_and_conditions) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+
+        mysqli_stmt_bind_param( $stmt, 'ssssssssssssi', $name, $email, $hash_password, $r_name, $r_email, $r_number, $contactnumber, $address, $industry, $branch, $description, $website, $tcpp);
+
+
+        if (mysqli_stmt_execute($stmt)) {
+            header("Location: ../Sign-in.html");
+        } else {
+            echo "<h3>Error: " . $stmt->error . "</h3>";
+        }
+
+        $stmt->close();
     }
-    $stmt->close();
-
-    $hash_password = password_hash($password, PASSWORD_BCRYPT);
-
-    $stmt = mysqli_prepare($connect, "INSERT INTO companies(company_name, company_email, password, representative_name, representative_email, representative_contact_number, company_contact_number, postal_address, industry_type, branch, company_description, company_website, agreed_terms_and_conditions) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
-
-    mysqli_stmt_bind_param( $stmt, 'ssssssssssssi', $name, $email, $hash_password, $r_name, $r_email, $r_number, $contactnumber, $address, $industry, $branch, $description, $website, $tcpp);
+    
 
 
-    if (mysqli_stmt_execute($stmt)) {
-        header("Location: ../Sign-in.html");
-    } else {
-        echo "<h3>Error: " . $stmt->error . "</h3>";
-    }
 
-    $stmt->close();
-}
 
 $connect->close();
 ?>
